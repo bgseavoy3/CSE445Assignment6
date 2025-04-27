ee10559cada0e6b8da1581f5265a473c3bc5acc8
DarshanHari19 <dhari4@asu.edu>
2025-04-26 22:03:01 -0700

    Add WordFilterService project

---
+++ WordFilterService/IService1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WordFilterService
+{
+    [ServiceContract]
+    public interface IWordFilterService
+    {
+        [OperationContract]
+        string WordFilter(string input);
+    }
+}
---
+++ WordFilterService/Service1.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+
+namespace WordFilterService
+{
+    // This class implements the IWordFilterService contract.
+    // Renaming the file is optional but recommended for clarity.
+    public class Service1 : IWordFilterService
+    {
+        public string WordFilter(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            // Define a list of stop words to filter out.
+            string[] stopWords = { "a", "an", "in", "on", "the", "is", "are", "am" };
+
+            // Split the input string into words using whitespace.
+            var words = input.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Filter out words that are in the stopWords list (case-insensitive).
+            var filteredWords = words.Where(word => !stopWords.Contains(word.ToLower())).ToArray();
+
+            // Join the remaining words back into a single string.
+            return string.Join(" ", filteredWords);
+        }
+    }
+}
